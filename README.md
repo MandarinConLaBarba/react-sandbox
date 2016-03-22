c516b53d40f7ecefa40df8f07e4edc7621eb9d3a
Mandarin Drummond <mandarind@gmail.com>
2016-03-22 12:29:42 -0400

    add another example

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ http://mandarinconlabarba.github.io/react-sandbox/cube-rotate/index.html
 An adaption of this really cool codepen: http://codepen.io/HugoGiraudel/pen/GLbca
 
 Basically uses hardware-accelerated CSS, React, and React-Motion.
+
+#### Cube Mouse Move
+
+http://mandarinconlabarba.github.io/react-sandbox/cube-rotate-mouse/index.html
+
+Same as the Cube Spinner, but tied to mouse movement.
---
+++ cube-rotate-mouse/app/components/stateless/App.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import {Grid} from 'bootstrap'
+
+const defaultMouseX = 0;
+const defaultMouseY = 0;
+
+const App = React.createClass({
+
+  getInitialState() {
+    return {
+      mouseX: defaultMouseX,
+      mouseY: defaultMouseY
+    }
+  },
+
+  render() {
+
+    let {mouseX, mouseY} = this.state;
+
+    let zSideTransform = 1;
+
+    if (mouseY > 100) {
+      zSideTransform = mouseY / 100;
+    }
+
+    return (
+      <Grid fluid className="cubeWrapper" onMouseMove={this._handleMouseMove}>
+        <Cube sideZ={zSideTransform} cubeYRotate={mouseX} cubeXRotate={mouseY}/>
+      </Grid>
+    );
+  },
+
+  _handleMouseMove(event) {
+
+    this.setState({
+      mouseX: event.pageX,
+      mouseY: event.pageY
+    });
+
+  }
+})
+
+
+const Cube = (props) => {
+
+  let {sideZ, cubeYRotate, cubeXRotate} = props
+
+  let frontStyle = {transform: `translateZ(${sideZ}em)`}
+  let backStyle = {transform: `rotateX(90deg) translateZ(${sideZ}em)`}
+  let rightStyle = {transform: `rotateY(90deg) translateZ(${sideZ}em)`}
+  let leftStyle = {transform: `rotateY(-90deg) translateZ(${sideZ}em)`}
+  let topStyle = {transform: `rotateX(-90deg) translateZ(${sideZ}em)`}
+  let bottomStyle = {transform: `rotateY(-180deg) translateZ(${sideZ}em)`}
+
+  let cubeStyle = {
+    transform: `rotateX(${cubeXRotate}deg) rotateY(${cubeYRotate}deg)`
+  }
+
+  return (
+    <div className="cube" style={cubeStyle}>
+      <div className="side" style={frontStyle}><span className="fa fa-hand-lizard-o"/></div>
+      <div className="side" style={backStyle}><span className="fa fa-hand-rock-o"/></div>
+      <div className="side" style={rightStyle}><span className="fa fa-hand-pointer-o"/></div>
+      <div className="side" style={leftStyle}><span className="fa fa-hand-stop-o"/></div>
+      <div className="side" style={topStyle}><span className="fa fa-hand-peace-o"/></div>
+      <div className="side" style={bottomStyle}><span className="fa fa-hand-spock-o"/></div>
+    </div>
+  );
+
+}
+
+export default App
